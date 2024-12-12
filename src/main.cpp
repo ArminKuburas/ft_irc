eed5a9ea4e55d544c31f6423c014c6bb4c602a60
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-12-12 09:32:19 +0200

    chore: saving branch progress

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@
  * 4. encrypted port or no?
  * 5. port range? ---------> 6667
  */
-bool	is_digit(std::string str)
+bool	isDigit(std::string str)
 {
 	for (char ch : str)
 	{
@@ -35,110 +35,128 @@ bool	is_digit(std::string str)
 	return (true);
 }
 
-int main(int argc, char **argv)
+bool	parsing(std::string port, std::string password)
 {
 	std::string usage = "usage: ./ircserv <port> <password>";
+	if (password.empty())
+	{
+		std::cout << "Error: password cannot be empty" << std::endl;
+		std::cout << usage << std::endl;
+		return (false);
+	} 
+	if (port.empty())
+	{
+		std::cout << "Error: cannot be empty" << std::endl;
+		std::cout << usage << std::endl;
+		return (false);
+	}
+	if (!isDigit(port))
+	{
+		std::cout << "Error: port must be only digits within range 1-65535" << std::endl;
+		std::cout << usage << std::endl;
+		return (false);
+	}
+	return (true);
+}
+
+int portConversion(std::string port)
+{
+	int port_number = std::stoi(port);
+	if (port_number < 1 && port_number > 65535)
+	{
+		std::cout << "Error: port number must be in the range 1-65535" << std::endl;
+		return (-1);
+	}
+	return (port_number);
+}
+
+int main(int argc, char **argv)
+{
 	if (argc == 3)
 	{
-		std::string port = argv[1];
-		std::string password = argv[2];
-		if (password.empty())
-		{
-			std::cout << "Error: password cannot be empty" << std::endl;
-			std::cout << usage << std::endl;
+		std::string port = argv[1], password = argv[2];
+		
+		if (parsing(port, password) == false)
 			return (EXIT_FAILURE);
-		} 
-		if (port.empty())
-		{
-			std::cout << "Error: cannot be empty" << std::endl;
-			std::cout << usage << std::endl;
+		int port_number = portConversion(port);
+		if (port_number == -1)
 			return (EXIT_FAILURE);
-		}
-		if (!is_digit(port))
-		{
-			std::cout << "Error: port must be only digits within range 1-65535" << std::endl;
-			std::cout << usage << std::endl;
-			return (EXIT_FAILURE);
-		}
-		int port_number = std::stoi(port);
-		if (port_number < 1 && port_number > 65535)
-		{
-			std::cout << "here" << std::endl;
-			std::cout << "Error: port number must be in the range 1-65535" << std::endl;
-			return (EXIT_FAILURE);
-		}
-		int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-		if (socket_fd == -1)
+		int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (server_fd < 0)
 		{
 			perror("Socket creation failed");
 			return (EXIT_FAILURE);
 		}
-		int flags = fcntl(socket_fd, F_GETFL);
+		int flags = fcntl(server_fd, F_GETFL);
 		if (flags == -1)
 		{
 			perror("fcntl(F_GETFL) failed");
-			close(socket_fd);
+			close(server_fd);
 			return (EXIT_FAILURE);
 		}
-		if (fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK) == -1)
+		if (fcntl(server_fd, F_SETFL, flags | O_NONBLOCK) == -1)
 		{
 			perror("fcntl(F_SETFL) failed");
-			close (socket_fd);
+			close (server_fd);
 			return (EXIT_FAILURE);
 		}
 		
 		std::cout << "Server is set to non-blocking mode" << std::endl;
 
-		struct sockaddr_in server_addr;
+		struct sockaddr_in server_addr, client_addr;
+		socklen_t client_addr_len = sizeof(client_addr);
 		memset(&server_addr, 0, sizeof(server_addr));
 		server_addr.sin_family = AF_INET;
 		server_addr.sin_port = htons(port_number);
 		server_addr.sin_addr.s_addr = INADDR_ANY;
+		char buffer[1024];
 
-		if (bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+		if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
 		{
 			perror("bind failed");
-			close(socket_fd);
+			close(server_fd);
 			return (EXIT_FAILURE);
 		}
 
-		if (listen(socket_fd, 100) == -1)
+		if (listen(server_fd, 5) == -1)
 		{
 			perror("listening failed");
-			close(socket_fd);
+			close(server_fd);
 			return (EXIT_FAILURE);
 		}
 
 		std::cout << "Server is listening in non-blocking mode" << std::endl;
+		std::cout << "Port: " << port << std::endl;
 		
-		int client_fd;
-		while (true) // change condition to signal handler later
+		int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+		if (client_fd < 0)
 		{
-			client_fd = accept(socket_fd, nullptr, nullptr);
-			if (client_fd == -1)
-			{
-				if (errno == EAGAIN || errno == EWOULDBLOCK)
-				{
-					std::cout << "No incoming connections, retrying...." << std::endl;
-					usleep(500000);
-					continue ;
-				}
-				else
-				{
-					perror("accept failed");
-					break ;
-				}
-			}
+			perror("accept failed");
+			close(server_fd);
+			return (EXIT_FAILURE);
 		}
+		
 		std::cout << "Client connected! File descriptor: " << client_fd << std::endl;
+		
+
+		ssize_t bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+		if (bytes_read < 0)
+		{
+			perror("read failure");
+			close(client_fd);
+			close(server_fd);
+			return (EXIT_FAILURE);
+		}
+		buffer[bytes_read] = '\0';
+
+		std::cout << "Message: " << buffer << std::endl;
 
 		// placeholder for client handling logic
 
 		close(client_fd);
-		std::cout << socket_fd << std::endl;
-		close(socket_fd);
+		close(server_fd);
 	}
 	else
-		std::cout << usage << std::endl;
+		std::cout << "usage: ./ircserv <port> <password>" << std::endl;
 	return (0);
 }
