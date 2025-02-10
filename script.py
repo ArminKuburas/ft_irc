30574dc79046c9c693b168cc69d92785f838d6aa
Pablo Markaide <pablo.markaide@gmail.com>
2025-02-10 14:09:55 +0200

    feat: ip as argument in the python script

--- script.py
+++ script.py
@@ -6,24 +6,24 @@
 #    By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/02/10 13:38:42 by akuburas          #+#    #+#              #
-#    Updated: 2025/02/10 13:43:48 by akuburas         ###   ########.fr        #
+#    Updated: 2025/02/10 14:09:20 by pmarkaid         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import subprocess
 import sys
 
-def open_irssi(nickname):
+def open_irssi(nickname, ip_address):
     command = (
-        f'gnome-terminal -- bash -c "irssi -c 127.0.0.1 -p 6697 -w 123 -n {nickname}; exec bash"'
+        f'gnome-terminal -- bash -c "irssi -c {ip_address} -p 6697 -w 123 -n {nickname}; exec bash"'
     )
     subprocess.Popen(command, shell=True)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("Usage: python3 script.py <nickname1> <nickname2> ...")
         sys.exit(1)
-    
-    nicknames = sys.argv[1:]
+    ip_address = sys.argv[1]
+    nicknames = sys.argv[2:]
     for nickname in nicknames:
-        open_irssi(nickname)
+        open_irssi(nickname, ip_address)
